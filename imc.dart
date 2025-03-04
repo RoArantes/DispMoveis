718c106822779132ae938d86e1cf0ba184e23cf7
Roberta Arantes <betaarantes@hotmail.com>
2025-03-04 15:52:08 -0300

    Add files via upload

---
+++ imc.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void main(){
+
+  print('Digite o peso: ');
+  double peso=double.parse(stdin.readLineSync()!);
+
+  print('Digite a altura: ');
+  double altura=double.parse(stdin.readLineSync()!);
+
+  double imc=peso/(altura*altura);
+
+  if(imc<18.5){
+    print('Abaixo do peso');
+  }else if(imc>=18.5 && imc<24.9){
+    print('Peso normal');
+  }else if(imc>=24.9 && imc<29.9){
+    print('Sobrepeso');
+  }else if(imc>=29.9 && imc<34.9){
+    print('Obesidade grau 1');
+  }else if(imc>=34.9 && imc<39.9){
+    print('Obesidade grau 2');
+  }else{
+    print('Obesidade grau 3');
+  }
+}
